7074e4fb60b3123d5b781f97d8f1449ac55c1abd
Adam Walzer <adamwalzer@gmail.com>
2017-03-09 16:13:38 -0500

    updates CORE-3312

---
+++ js/states.js
@@ -0,0 +1,32 @@
+import loadAssets from 'shared/phaser/methods/load_assets/0.1';
+import setGameStage from 'shared/phaser/methods/set_game_stage/0.1';
+
+import opts from './opts';
+import getState from './get_state';
+
+export default _.reduce(opts, (a, v, k) => {
+    a[k + 1] = getState(v);
+    return a;
+}, {
+    default: {
+        preload: function () {
+            loadAssets.call(this, 'image', [
+            ['sky', `${MEDIA.IMAGE}level01.panorama.jpg`],
+            ]);
+        },
+        create: function () {
+            setGameStage.call(this, {
+                width: 30000,
+                height: 740,
+                top: -200,
+            });
+            this.helpers.makeBackground.call(this);
+        },
+        update: function () {
+            if (!this.shouldUpdate) {
+                this.shouldUpdate = true;
+                this.emitEvent({ ready: true });
+            }
+        },
+    }
+});
